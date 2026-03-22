90473a551cc9de5d425f65620a15767158c25c7d
Hanzala Ghani <hanzalaghaniabdulghani@gmail.com>
2026-03-22 12:34:49 +0500

    Project setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="bg-gray-200 text-3xl font-bold text-center p-4">
+
+       lawlelulia-platform
+      </div>
+    </>
+  )
+}
+
+export default App
