e33ae9dbc4d16c125a08d5512dd585d9a668a9b2
Hanzala Ghani <hanzalaghaniabdulghani@gmail.com>
2026-03-22 13:14:39 +0500

    conected DB

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,12 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import connectDB from "./config/db.js";
 
 dotenv.config();
+
+// CONNECT DB
+connectDB();
 
 const app = express();
 
@@ -10,7 +14,7 @@ app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("API is running...");
+  res.send("API running...");
 });
 
 const PORT = process.env.PORT || 5000;
