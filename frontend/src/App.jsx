7db251f2f1998e14c38ece34392d79c421af079b
muhammadanascode <anassohal34343@gmail.com>
2026-03-23 22:17:08 +0500

    feat: added about us page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import Home from './pages/Home';
 import Navbar from './components/Navbar';
 import Sidebar from './components/Sidebar';
 import Profile from './pages/Profile';
+import AboutUs from './pages/Aboutus';
 
 function App() {
 
@@ -24,6 +25,7 @@ function App() {
               <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/profile" element={<Profile />} />
+                <Route path="/aboutus" element={<AboutUs />} />
               </Routes>
             </div>
 
