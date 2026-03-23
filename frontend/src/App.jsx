4435b46d05a99c61937a42334dc4d35d34aca3e1
muhammadanascode <anassohal34343@gmail.com>
2026-03-23 16:25:34 +0500

    feat: added sidebar and adjusted layout

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,16 +2,31 @@ import './App.css'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from './pages/Home';
 import Navbar from './components/Navbar';
+import Sidebar from './components/Sidebar';
 
 function App() {
 
   return (
     <>
       <BrowserRouter>
-      <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
+        <div className="flex min-h-screen">
+          {/* Sidebar */}
+          <Sidebar />
+
+          {/* Main content area */}
+          <div className="flex-1 flex flex-col">
+            {/* Navbar */}
+            <Navbar />
+
+            {/* Pages */}
+            <div className="flex-1 p-4 bg-gray-50">
+              <Routes>
+                <Route path="/" element={<Home />} />
+              </Routes>
+            </div>
+
+          </div>
+        </div>
       </BrowserRouter>
     </>
   )
