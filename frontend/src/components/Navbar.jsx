ccd41c45baba5bcc56bbeee0a119a08ad0b68947
muhammadanascode <anassohal34343@gmail.com>
2026-03-23 15:23:41 +0500

    feat: designed full navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,9 +1,30 @@
+import { Link } from "react-router-dom";
+
 const Navbar = () => {
     return (
-        <nav className="w-full bg-black">
-            Navbar
+        <nav className="w-full bg-[#0A0F1C] flex p-2 justify-between items-center">
+            {/* Logo */}
+            <div className="flex items-center space-x-2">
+                <div className="w-8 h-8 rounded-full bg-white flex justify-center items-center font-semibold text-2xl">
+                    L
+                </div>
+                <div className="text-white mt-1">awlelulia</div>
+            </div>
+
+            {/* Nav links */}
+            <div className="flex space-x-6 text-white text-xs font-semibold">
+                <Link to="/" className="hover:text-gray-300">Home</Link>
+                <Link to="/appointments" className="hover:text-gray-300">Appointments</Link>
+                <Link to="/bookings" className="hover:text-gray-300">Bookings</Link>
+                <Link to="/about" className="hover:text-gray-300">About Us</Link>
+            </div>
+
+            {/* Welcome Text */}
+            <div className="text-white mr-4">
+                <p>Welcome, <b>Admin</b></p>
+            </div>
         </nav>
-    )
-}
+    );
+};
 
 export default Navbar;
