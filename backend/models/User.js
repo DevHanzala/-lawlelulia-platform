26fbc7fd8e23483b64df814b3955c0989666357e
muhammadanascode <anassohal34343@gmail.com>
2026-03-26 16:27:26 +0500

    feat: created model folder and added User Model

---
+++ backend/models/User.js
@@ -0,0 +1,37 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    fullName: {
+        type: String,
+        required: [true, "Full name is required"],
+        minlength: [3, "Full name must be at least 3 characters"],
+        trim: true,
+        validate: {
+            validator: function (v) {
+                return /^[A-Za-z\s]+$/.test(v); // only letters + spaces
+            },
+            message: "Full name must contain only letters"
+        }
+    },
+
+    email: {
+        type: String,
+        required: [true, "Email is required"],
+        unique: true,
+        lowercase: true,
+        trim: true,
+        match: [
+            /^\S+@\S+\.\S+$/,
+            "Please use a valid email address"
+        ]
+    },
+
+    password: {
+        type: String,
+        required: [true, "Password is required"],
+        minlength: [8, "Password must be at least 8 characters"]
+    }
+
+}, { timestamps: true });
+
+export default mongoose.model("User", userSchema);
