d9c9616efbae6a7d4dd7978ae7c4d095c6c3e743
muhammadanascode <anassohal34343@gmail.com>
2026-03-26 23:21:13 +0500

    feat: added route to veirfy otp

--- backend/routes/authRoute.js
+++ backend/routes/authRoute.js
@@ -1,11 +1,13 @@
 import express from "express";
 import {
   signUpOtp,
+  verifySignUpToken
 } from "../controllers/authController.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 
 const router = express.Router();
 
 router.post("/signup-sendotp", asyncHandler(signUpOtp));
+router.post("/signup-verifytoken", asyncHandler(verifySignUpToken));
 
 export default router;
