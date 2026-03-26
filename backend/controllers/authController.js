b6da30a1344c78e93801df0ee1b4af80b03d844d
muhammadanascode <anassohal34343@gmail.com>
2026-03-26 23:20:57 +0500

    feat: verify signup controller

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -2,9 +2,16 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 import * as authService from "../services/authService.js"
 import { success } from "../utils/apiResponse.js";
 
-// Route: Signup
+// Route: Signup sends otp
 export const signUpOtp = asyncHandler(async (req, res) => {
     const { fullname, email, password } = req.body;
     const data = await authService.signUpOtp(fullname, email, password);
     return success(res, "OTP sent", data);
+})
+
+//Route: verify signup otp token
+export const verifySignUpToken = asyncHandler(async (req, res) => {
+    const { email, otp } = req.body;
+    const data = await authService.verifySignUpToken(email, otp);
+    return success(res, "OTP verified successfully", data);
 })
