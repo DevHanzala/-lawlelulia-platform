361b3fe571537787827472f572008b6490bde499
muhammadanascode <anassohal34343@gmail.com>
2026-03-26 16:25:47 +0500

    feat: created method for httperror and added global exception handler

---
+++ backend/exception/globalExceptionHandler.js
@@ -0,0 +1,15 @@
+import { HttpError } from "./HttpError.js";
+import { error as sendError } from "../utils/apiResponse.js";
+
+const globalExceptionHandler = (err, req, res, next) => {
+
+    if (err instanceof HttpError) {
+        // Use statusCode and message from your HttpError
+        return sendError(res, err.message, err.statusCode);
+    }
+
+    // fallback for unknown errors
+    return sendError(res, "Internal Server Error", 500);
+};
+
+export default globalExceptionHandler;
