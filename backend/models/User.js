08162c57e490664db9a97d447d90aae16f279f46
muhammadanascode <anassohal34343@gmail.com>
2026-03-28 21:46:42 +0500

    feat: Added role field in user

--- backend/models/User.js
+++ backend/models/User.js
@@ -35,6 +35,12 @@ const userSchema = new mongoose.Schema({
     isVerified: {
         type: Boolean,
         default: false
+    },
+
+    role: {
+        type: String,
+        enum: ["user", "admin"],
+        default: "user"
     }
 
 }, { timestamps: true });
