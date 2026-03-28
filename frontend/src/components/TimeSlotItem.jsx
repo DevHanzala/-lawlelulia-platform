b19b9e48d7fe69e183f527a72300915cefdb0c57
muhammadanascode <anassohal34343@gmail.com>
2026-03-28 16:21:02 +0500

    feat: changed bg and imprved font size

--- frontend/src/components/TimeSlotItem.jsx
+++ frontend/src/components/TimeSlotItem.jsx
@@ -2,13 +2,13 @@ import { FaTrash } from "react-icons/fa";
 
 const TimeSlotItem = ({ time, onDelete }) => {
     return (
-        <div className="flex justify-between items-center bg-white rounded-lg p-2 shadow-sm">
-            <span className="text-gray-700">{time}</span>
+        <div className="flex justify-between items-center bg-gray-100 rounded-lg p-2 shadow-sm mt-2">
+            <span className=" text-sm text-gray-700 font-medium">{time}</span>
             <button
                 onClick={onDelete}
                 className="flex items-center gap-1 px-2 py-1 text-sm bg-red-100 text-red-600 rounded hover:bg-red-200"
             >
-                <FaTrash /> Delete
+                <FaTrash />
             </button>
         </div>
     );
