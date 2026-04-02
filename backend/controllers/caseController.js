cb63cd84b9f6be632dff2bdba4867c2e8e544b64
muhammadanascode <anassohal34343@gmail.com>
2026-04-02 18:20:24 +0500

    feat: created controller

---
+++ backend/controllers/caseController.js
@@ -0,0 +1,18 @@
+import { asyncHandler } from "../utils/asyncHandler.js";
+import * as caseService from "../services/caseService.js";
+import { success } from "../utils/apiResponse.js";
+
+// Controller: Get all cases for the authenticated user
+export const getCasesByUserId = asyncHandler(async (req, res) => {
+    const user = req.user;
+    const cases = await caseService.getCasesByUserId(user);
+    return success(res, "Cases fetched successfully", cases);
+})
+
+// Controller: Create a new case for the authenticated user
+export const createNewCase = asyncHandler(async (req, res) => {
+    const { caseTitle, caseDescription } = req.body;
+    const user = req.user;
+    const newCase = await caseService.createNewCase(caseTitle, caseDescription, user);
+    return success(res, "Case created successfully", newCase);
+})
