16ecd44bc408d518f7041e4b3d77f263665986fd
muhammadanascode <anassohal34343@gmail.com>
2026-04-03 16:31:04 +0500

    feat: created a service file to  for uploading adding and deleting docs

---
+++ backend/services/fileService.js
@@ -0,0 +1,47 @@
+import fs from 'fs';
+import { drive } from '../config/googleDrive.js';
+import dotenv from "dotenv";
+
+dotenv.config();
+
+// Service: Upload file to Google Drive
+export const uploadToDrive = async (file) => {
+    try {
+        const response = await drive.files.create({
+            requestBody: {
+                name: file.originalname,
+                parents: [process.env.FOLDER_ID]
+            },
+            media: {
+                mimeType: file.mimetype,
+                body: fs.createReadStream(file.path)
+            }
+        });
+
+        // Make file public
+        await drive.permissions.create({
+            fileId: response.data.id,
+            requestBody: {
+                role: 'reader',
+                type: 'anyone'
+            }
+        });
+
+        // Delete local file
+        fs.unlinkSync(file.path);
+
+        return {
+            fileId: response.data.id,
+            url: `https://drive.google.com/file/d/${response.data.id}/view`
+        };
+
+    } catch (error) {
+
+        //  delete file even if upload fails
+        if (file?.path && fs.existsSync(file.path)) {
+            fs.unlinkSync(file.path);
+        }
+
+        throw new Error("File upload failed", 500);
+    }
+};
