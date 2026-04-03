894bfdd68bc67ca04c4fa39f42f22dab01948c95
muhammadanascode <anassohal34343@gmail.com>
2026-04-03 16:47:57 +0500

    feat: added a middleware to parse files and attach it in req

--- backend/routes/appointmentRoute.js
+++ backend/routes/appointmentRoute.js
@@ -2,16 +2,19 @@ import express from "express";
 import {
   createAppointment,
   updateAppointmentStatus,
-    getUserAppointmentHistory,
-    getUserFutureAppointments
+  getUserAppointmentHistory,
+  getUserFutureAppointments
 } from "../controllers/appointmentController.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { protect } from "../middleware/authMiddleware.js";
+import { fileUpload } from "../middleware/fileUploadMiddleware.js";
 
 const router = express.Router();
 
 // Appointments
-router.post("/create", protect, asyncHandler(createAppointment));
+
+//Expects a file upload with the key "file" in the request body for creating an appointment
+router.post("/create", protect, fileUpload.single("file"), asyncHandler(createAppointment));
 router.put("/:appointmentId", protect, asyncHandler(updateAppointmentStatus));
 router.get("/history", protect, asyncHandler(getUserAppointmentHistory));
 router.get("/future", protect, asyncHandler(getUserFutureAppointments));
