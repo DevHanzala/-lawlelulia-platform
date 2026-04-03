dfce3071cc484287226dc032568c73ccfc08b1a1
muhammadanascode <anassohal34343@gmail.com>
2026-04-03 16:29:47 +0500

    feat:created to middleware to convert file in usable object

---
+++ backend/middleware/uploadMiddleware.js
@@ -0,0 +1,33 @@
+import multer from "multer";
+import path from "path";
+
+// Storage config
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, "uploads/"); // make sure this folder exists
+    },
+    filename: (req, file, cb) => {
+        const uniqueName = file.originalname + "-" + Date.now();
+        cb(null, uniqueName);
+    }
+});
+
+// File filter 
+const fileFilter = (req, file, cb) => {
+    const allowedTypes = ["application/pdf", "image/png", "image/jpeg"];
+
+    if (allowedTypes.includes(file.mimetype)) {
+        cb(null, true);
+    } else {
+        cb(new Error("Only PDF, PNG, JPG allowed"), false);
+    }
+};
+
+// Multer instance
+export const upload = multer({
+    storage,
+    fileFilter,
+    limits: {
+        fileSize: 5 * 1024 * 1024 // 5MB
+    }
+});
