a2068e7d0f784ccaca0083bd0c6b5a8e84c5157d
muhammadanascode <anassohal34343@gmail.com>
2026-04-04 19:27:48 +0500

    feat: created store for case handling

---
+++ frontend/src/store/caseStore.js
@@ -0,0 +1,39 @@
+import { create } from "zustand";
+import {
+    createCaseApi,
+    getCasesApi
+} from "../api/case.api";
+
+const useCaseStore = create((set) => ({
+    cases: [],
+    loading: false,
+    error: "",
+
+    // Create new case
+    createCase: async (title, description) => {
+        set({ loading: true, error: "" });
+        try {
+            const newCaseRes = await createCaseApi({ caseTitle: title, caseDescription: description });
+            set((state) => ({ cases: [...state.cases, newCaseRes.data.data], loading: false }));
+            return { success: true };
+        } catch (error) {
+            console.log("Error creating case:", error);
+            set({ error: error.message, loading: false });
+            return { success: false, message: error.message };
+        }
+    },
+
+    // get Cases ( user ID )
+    getCases: async () => {
+        set({ loading: true, error: "" });
+        try {
+            const res = await getCasesApi();
+            set({ cases: res.data.data, loading: false });
+        } catch (error) {
+            set({ error: error.message, loading: false });
+        }
+    }
+
+}));
+
+export default useCaseStore;
