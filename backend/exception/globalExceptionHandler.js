0aa409e3210cd362fdc0c6c07165b9727553c817
muhammadanascode <anassohal34343@gmail.com>
2026-04-04 20:23:05 +0500

    feat: printing server logs

--- backend/exception/globalExceptionHandler.js
+++ backend/exception/globalExceptionHandler.js
@@ -7,7 +7,7 @@ const globalExceptionHandler = (err, req, res, next) => {
         // Use statusCode and message from your HttpError
         return sendError(res, err.message, err.statusCode);
     }
-    console.log("Error: ", err);   //log server errors
+    console.log("Error: ", err.message);   //log server errors
     // fallback for unknown errors
     return sendError(res, "Internal Server Error", 500);
 };
