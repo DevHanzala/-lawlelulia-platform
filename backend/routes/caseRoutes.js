c28569004d203c174cc1956e4d08a8b055c2fb41
muhammadanascode <anassohal34343@gmail.com>
2026-04-06 14:57:58 +0500

    feat:  Created route for it

--- backend/routes/caseRoutes.js
+++ backend/routes/caseRoutes.js
@@ -1,7 +1,8 @@
 import express from "express";
 import {
   getCasesByUserId,
-  createNewCase
+  createNewCase,
+  getCasesWithAppointments
 } from "../controllers/caseController.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { protect } from "../middleware/authMiddleware.js";
@@ -10,6 +11,7 @@ const router = express.Router();
 
 router.post("/createCase", protect, asyncHandler(createNewCase));
 router.get("/getCases", protect, asyncHandler(getCasesByUserId));
+router.get("/getCasesWithAppointments", protect, asyncHandler(getCasesWithAppointments));
 
 
 export default router;
