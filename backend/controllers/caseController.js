82d2c1c74e6acb075e1907cbf23fc2791a7a00c6
muhammadanascode <anassohal34343@gmail.com>
2026-04-06 14:57:42 +0500

    feat: created controller

--- backend/controllers/caseController.js
+++ backend/controllers/caseController.js
@@ -16,3 +16,10 @@ export const createNewCase = asyncHandler(async (req, res) => {
     const newCase = await caseService.createNewCase(caseTitle, caseDescription, user);
     return success(res, "Case created successfully", newCase);
 })
+
+// Controller: Get Case along with its appointment details
+export const getCasesWithAppointments = asyncHandler(async (req, res) => {
+    const user = req.user;
+    const casesWithAppointments = await caseService.getCasesWithAppointments(user);
+    return success(res, "Cases with appointments fetched successfully", casesWithAppointments);
+});
